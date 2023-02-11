df1ff80e7fd03950bee4a0ca940ac1c20951d8f8
AnandBharati <anandbharati0402@gmail.com>
2023-02-11 14:26:03 +0530

    final commit

--- client/src/helpers/ApiDomain.js
+++
@@ -1,4 +0,0 @@
-const domain= process.env.API_DOMAIN || 'https://sore-gray-oyster-coat.cyclic.app';
-
-
-export default domain;
---
+++ client/src/helpers/ApiDomain.jsx
@@ -0,0 +1,5 @@
+const domain= process.env.API_DOMAIN || 'gggg'
+//'https://sore-gray-oyster-coat.cyclic.app';
+
+
+export default domain;
